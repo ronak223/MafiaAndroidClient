1df6cb67d54da9372597a76faaa390338ab9baa3
Ronak <ronak223@gmail.com>
2013-11-15 23:42:54 -0500

    Lobby activity shows current players now

--- gen/edu/wm/cs/mafia/R.java
+++ gen/edu/wm/cs/mafia/R.java
@@ -24,23 +24,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f080010;
-        public static final int action_settings=0x7f08000f;
-        public static final int confirm_registration_button=0x7f08000e;
+        public static final int action_forgot_password=0x7f080011;
+        public static final int action_settings=0x7f080010;
+        public static final int confirm_registration_button=0x7f08000f;
         public static final int create_or_join_game_button=0x7f080003;
         public static final int day_night_freq_picker=0x7f080004;
         public static final int day_night_freq_text=0x7f080005;
         public static final int game_availability_text=0x7f080002;
         public static final int game_selection_text=0x7f080001;
-        public static final int login_button=0x7f080008;
-        public static final int password_input=0x7f080006;
-        public static final int reg_email=0x7f08000d;
-        public static final int reg_password=0x7f08000b;
-        public static final int reg_reenter_password=0x7f08000c;
-        public static final int reg_userid=0x7f08000a;
-        public static final int register_button=0x7f080009;
+        public static final int lobby_names_listview=0x7f080006;
+        public static final int login_button=0x7f080009;
+        public static final int password_input=0x7f080007;
+        public static final int reg_email=0x7f08000e;
+        public static final int reg_password=0x7f08000c;
+        public static final int reg_reenter_password=0x7f08000d;
+        public static final int reg_userid=0x7f08000b;
+        public static final int register_button=0x7f08000a;
         public static final int relative_layout_game_selection=0x7f080000;
-        public static final int userid_input=0x7f080007;
+        public static final int userid_input=0x7f080008;
     }
     public static final class layout {
         public static final int activity_game_start=0x7f030000;
--- src/edu/wm/cs/mafia/LobbyActivity.java
+++ src/edu/wm/cs/mafia/LobbyActivity.java
@@ -1,8 +1,21 @@
 package edu.wm.cs.mafia;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.json.parsers.JSONParser;
+import com.json.parsers.JsonParserFactory;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 public class LobbyActivity extends Activity {
 
@@ -10,6 +23,51 @@ public class LobbyActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lobby);
+		
+		//getting listView by resource ID, and context
+		final ListView lv = (ListView)findViewById(R.id.lobby_names_listview);
+		final Context cur_context = getApplicationContext();
+		
+		//loading spinner
+		final ProgressDialog progress = new ProgressDialog(this);
+    	progress.setTitle("Loading");
+    	progress.setMessage("Please wait...");
+    	progress.show();
+    	
+    	//setting up JasonFactory
+    	JsonParserFactory factory=JsonParserFactory.getInstance();
+    	final JSONParser parser=factory.newJsonParser();
+    	
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.setBasicAuth("specialkeythatnoonewilleverknow", "specialerpasswordisawesome");
+		client.get("http://mafia-web-service.herokuapp.com/getAllPlayers", new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(String response){
+				Map jsonData=parser.parseJson(response);
+				Map rootJson= (Map) jsonData.get("root");
+				List al= (List) jsonData.get("response");
+				
+				//initializing list for player names
+				ArrayList<String> player_name_list = new ArrayList<String>();
+				
+				for(int i = 0; i < al.size(); i++){
+					String userID=(String) ((Map)al.get(i)).get("userID");
+					player_name_list.add(userID);
+					
+				}
+				//setting ListView to array of player userIDs that are currently queued up to play
+				ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(cur_context,android.R.layout.simple_list_item_1, player_name_list);
+				lv.setAdapter(arrayAdapter); 
+				
+				//dismissing progress bar
+				progress.dismiss();
+			}
+		});	
+		
+		//TODO Add automatic updating of players to listView as they join
+		//TODO button to allow ADMIN to start game, getting coords of remaining players and starting game, as well as setting werewolf/townsperson ratio (through web service)
+		//TODO players that are not admin must wait for admin to start game before playing
+		//TODO allow only ADMIN to restart game frequency here; once game is started, it is set
 	}
 
 	@Override
